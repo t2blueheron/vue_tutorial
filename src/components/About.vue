24e1765e46e202f5cdea51a1d35f405e88f4fe80
Carol Harrison <carol.harrison@goenergistics.com>
2018-07-19 12:43:34 -0400

    add delete

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hello {{their_name}}, This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,7 +11,10 @@
      
      <ul>
        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-          <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+          <li v-for="(data, index) in skills" :key='index'>
+            {{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+            </li>
         </transition-group>
      </ul>
      <p>These are the skills that you possess.</p>
@@ -35,6 +38,9 @@ export default {
     addSkill () {
       this.skills.push({skill: this.skill})
       this.skill = '';
+    },
+    remove(id) {
+      this.skills.splice(id,1);
     }
   }
 }
@@ -42,6 +48,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 .holder {
     background: #fff;
